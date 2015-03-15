6b67fab499e7aac64cb2095c8df0d484aff9018e
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2015-03-15 17:16:10 +0900

    impl title in TodoEntity

--- app/src/main/java/jp/co/mixi/training/android/todo/entity/TodoEntity.java
+++ app/src/main/java/jp/co/mixi/training/android/todo/entity/TodoEntity.java
@@ -1,7 +1,23 @@
 package jp.co.mixi.training.android.todo.entity;
 
 /**
+ * TODOを表現するためのEntity
  * Created by Hideyuki.Kikuma on 2015/03/15.
  */
 public class TodoEntity {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String toString(){
+        return title;
+    }
+
+    //TODO builderパターンにする
 }

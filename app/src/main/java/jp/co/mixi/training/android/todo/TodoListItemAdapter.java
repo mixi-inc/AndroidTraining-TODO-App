013325eb7fd44632ffa46a92de11da6f7cd12421
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2015-03-15 18:22:19 +0900

    素朴なListviewAdapterの実装

--- app/src/main/java/jp/co/mixi/training/android/todo/TodoListItemAdapter.java
+++ app/src/main/java/jp/co/mixi/training/android/todo/TodoListItemAdapter.java
@@ -1,7 +1,10 @@
 package jp.co.mixi.training.android.todo;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -13,5 +16,16 @@ import jp.co.mixi.training.android.todo.entity.TodoEntity;
 public class TodoListItemAdapter extends ArrayAdapter<TodoEntity> {
     public TodoListItemAdapter(Context context, int resource, List<TodoEntity> objects) {
         super(context, resource, objects);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = View.inflate(getContext(), R.layout.todo_list_item, null);
+        }
+        TodoEntity entity = getItem(position);
+        TextView title = (TextView) convertView.findViewById(R.id.title);
+        title.setText(entity.getTitle());
+
+        return convertView;
     }
 }

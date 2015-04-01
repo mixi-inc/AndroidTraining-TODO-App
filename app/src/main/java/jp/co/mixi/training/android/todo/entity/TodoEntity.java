d8c1eace08190b899e939e5ea00b1e1738f175da
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2015-04-02 02:02:14 +0900

    TodoEntityにfromJsonとtoJsonを追加
    - シリアライズ、デシリアライズにgsonを導入

--- app/src/main/java/jp/co/mixi/training/android/todo/entity/TodoEntity.java
+++ app/src/main/java/jp/co/mixi/training/android/todo/entity/TodoEntity.java
@@ -1,10 +1,14 @@
 package jp.co.mixi.training.android.todo.entity;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 /**
  * TODOを表現するためのEntity
  * Created by Hideyuki.Kikuma on 2015/03/15.
  */
 public class TodoEntity {
+    private static final Gson GSON = new GsonBuilder().create();
     private String title;
 
     public String getTitle() {
@@ -15,9 +19,16 @@ public class TodoEntity {
         this.title = title;
     }
 
-    public String toString(){
+    public String toString() {
         return title;
     }
 
+    public String toJson() {
+        return GSON.toJson(this);
+    }
+
+    public static TodoEntity fromJson(String json) {
+        return GSON.fromJson(json, TodoEntity.class);
+    }
     //TODO builderパターンにする
 }

71f2094f879e11aed87871df6e5267489420dd1f
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2015-05-27 02:52:29 +0900

    add ReminderNotificationSender class

---
+++ app/src/main/java/jp/co/mixi/training/android/todo/ReminderNotificationSender.java
@@ -0,0 +1,17 @@
+package jp.co.mixi.training.android.todo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ReminderNotificationSender extends BroadcastReceiver {
+    public ReminderNotificationSender() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}

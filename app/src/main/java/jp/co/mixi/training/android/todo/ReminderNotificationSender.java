c85708e3401778dd6860a5f3eba9e0dc684a8a55
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2015-05-27 03:26:46 +0900

    add intent builder

--- app/src/main/java/jp/co/mixi/training/android/todo/ReminderNotificationSender.java
+++ app/src/main/java/jp/co/mixi/training/android/todo/ReminderNotificationSender.java
@@ -3,8 +3,15 @@ package jp.co.mixi.training.android.todo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 
 public class ReminderNotificationSender extends BroadcastReceiver {
+    private static String CONTENT_TITLE_NAME = "content_title";
+    private static String CONTENT_TEXT_NAME = "content_text";
+    private static String SMALL_ICON_NAME = "small_icon";
+    private static String TICKER_NAME = "ticker";
+    private static String ID_NAME = "todo_id";
+
     public ReminderNotificationSender() {
     }
 
@@ -13,5 +20,54 @@ public class ReminderNotificationSender extends BroadcastReceiver {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public static class IntentBuilder {
+        private final Context context;
+
+        private String title;
+        private String text;
+        private int smallIconId;
+        private String ticker;
+        private int id;
+
+        public IntentBuilder(@NonNull Context context) {
+            this.context = context;
+        }
+
+        public Intent build() {
+            Intent intent = new Intent(context, ReminderNotificationSender.class);
+            intent.putExtra(CONTENT_TITLE_NAME, title)
+                    .putExtra(ID_NAME, id)
+                    .putExtra(CONTENT_TEXT_NAME, text)
+                    .putExtra(SMALL_ICON_NAME, smallIconId)
+                    .putExtra(TICKER_NAME, ticker);
+            return intent;
+        }
+
+        public IntentBuilder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public IntentBuilder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public IntentBuilder setText(String text) {
+            this.text = text;
+            return this;
+        }
+
+        public IntentBuilder setSmallIconId(int smallIconId) {
+            this.smallIconId = smallIconId;
+            return this;
+        }
+
+        public IntentBuilder setTicker(String ticker) {
+            this.ticker = ticker;
+            return this;
+        }
     }
 }

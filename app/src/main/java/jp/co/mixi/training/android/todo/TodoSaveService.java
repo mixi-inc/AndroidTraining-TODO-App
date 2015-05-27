364e72ebba1c9185294f9b29655e3be65d0f2b59
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2015-05-28 02:20:48 +0900

    todoの保存時に通知の予約を入れる

--- app/src/main/java/jp/co/mixi/training/android/todo/TodoSaveService.java
+++ app/src/main/java/jp/co/mixi/training/android/todo/TodoSaveService.java
@@ -1,8 +1,12 @@
 package jp.co.mixi.training.android.todo;
 
+import android.annotation.TargetApi;
+import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
@@ -57,15 +61,42 @@ public class TodoSaveService extends IntentService {
      * Handle action Save in the provided background thread with the provided
      * parameters.
      */
+    @TargetApi(Build.VERSION_CODES.KITKAT)
     private void handleActionSave(String json) {
         Log.v(TAG, "json:" + json);
 
         TodoEntity entity = TodoEntity.fromJson(json);
         TodoOpenHelper helper = new TodoOpenHelper(this);
+        long id = entity.getId();
         if (helper.isExist(entity)) {
             helper.updateTodo(entity);
         } else {
-            long id = helper.insertTodo(entity);
+            id = helper.insertTodo(entity);
+        }
+
+        // 本来ならすでに設定されているが、締め切りを消した場合はAlarmManagerから消す処理とかも入れた方がいい
+        if (entity.getDeadline() != null) {
+            Intent intent = new ReminderNotificationSender.IntentBuilder(this)
+                    .setTitle(entity.getTitle())
+                            // intのmax値は2147483647なので、そんな数のTODOをこなすのは人類には不可能なのでダウンキャストしても大丈夫と信じる　
+                    .setId((int) id)
+                            // todoに種別を持たせてそれごとにアイコン設定とかあってもいいかもしれないけど、今回は用意してないので固定
+                    .setSmallIconId(R.drawable.ic_launcher)
+                    .setTicker(entity.getTitle())
+                            // 詳細とかあれば、ここで設定してあげてもいい気がする
+                    .setText(entity.getTitle())
+                    .build();
+            AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            // kitkat以前ならam.setでOK
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                // 締め切りを過ぎたら通知が出る
+                // 多分本来はリマインダーとして事前になるように設定すべき
+                am.setExact(AlarmManager.RTC_WAKEUP, entity.getDeadline().getTime(), pendingIntent);
+            } else {
+                am.set(AlarmManager.RTC_WAKEUP, entity.getDeadline().getTime(), pendingIntent);
+            }
+
         }
 
         LocalBroadcastManager manager = LocalBroadcastManager.getInstance(this);
